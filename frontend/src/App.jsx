ff9f6ae0ad4c00bdf0b752de18eb687e2462ede9
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-14 17:18:22 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">¡Hola React + Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App
