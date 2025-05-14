43425c1d312675a9677aa8cff758cb58b22bd494
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-14 18:27:30 +0200

    Instala Flask-SQLAlchemy

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+# app.config.from_pyfile('config.py')
+# db = SQLAlchemy(app)       
 
 @app.route('/')
 def home():
     return 'API OK'
 
 if __name__ == '__main__':
+    # Crear las tablas solo la primera vez
+    # Descomenta estas líneas solo la primera vez que inicias la aplicación
+    # with app.app_context():                #  ← necesario para acceder al contexto de Flask
+    #     db.create_all()                    #  ← crea las tablas definidas en los modelos
     app.run(debug=True)
